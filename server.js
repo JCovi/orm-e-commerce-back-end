a1200029c692c3b3b7dcd4c25903f6e9e18c2c59
JCovi <joshpcovington@gmail.com>
2024-01-15 17:39:20 -0700

    final adjustments

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-require('dotenv').config();
+
 
 // import sequelize connection
+const sequelize = require('../orm-e-commerce-back-end/config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({force:false}).then(()=>{
+  
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+  });
